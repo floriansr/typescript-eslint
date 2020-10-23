909dd1bd76a5aa8b85ba82f008b2a0ffee44e43d
floriansr <sueurfloriansf@gmail.com>
2020-10-23 13:11:57 +0200

    [create] next-eslint-prettier app

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+    console.log('MyApp -> pageProps', pageProps);
+
+    return <Component {...pageProps} />;
+}
+
+export default MyApp;
