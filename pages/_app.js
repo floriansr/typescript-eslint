608ac02412ba0a7fdc578b438a08c3f98088faf0
floriansr <sueurfloriansf@gmail.com>
2020-10-23 13:17:37 +0200

    [refacto] js files to ES6

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-    console.log('MyApp -> pageProps', pageProps);
+const MyApp = ({ Component, pageProps }) => {
+  console.log('MyApp -> pageProps', pageProps);
 
-    return <Component {...pageProps} />;
-}
+  return <Component {...pageProps} />;
+};
 
 export default MyApp;
